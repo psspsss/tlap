3711f3976c08b24d1c0c88c88d61c1aa243416bb
vinayak majhi <vinayak.djp@gmail.com>
2025-06-03 22:22:55 +0530

    3 questions done

--- chap2Ques.cpp
+++ chap2Ques.cpp
@@ -5,27 +5,27 @@
 //   ####
 //    ##
 
-// #include<bits/stdc++.h>
+#include<bits/stdc++.h>
 
-// using namespace std;
+using namespace std;
 
-// int main(){
+int main(){
 
-//     for(int i =0; i<4;i++){
+    for(int i =0; i<4;i++){
 
-//         for(int j= 0; j<8;j++){
-//             if(j<i){
-//                 cout<<" ";
-//             }
+        for(int j= 0; j<8;j++){
+            if(j<i){
+                cout<<" ";
+            }
 
-//             else if( j < 8 - i)
-//             cout<<"*";
-//         }
-//         cout<<endl;
-//     } 
+            else if( j < 8 - i)
+            cout<<"*";
+        }
+        cout<<endl;
+    } 
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 // Q2.2
@@ -37,34 +37,34 @@ using namespace std;
 int main(){
 
 
-    // for(int n=0; n<4;n++){
+    for(int n=0; n<4;n++){
 
-    //         int numstars = 2* (n+1); 
-    //         int spaces = (8-numstars)/2;
+            int numstars = 2* (n+1); 
+            int spaces = (8-numstars)/2;
             
-    //     for(int m=0;m<spaces;m++){
-    //         cout<<" ";
-    //     }
+        for(int m=0;m<spaces;m++){
+            cout<<" ";
+        }
 
-    //     for(int m=0;m<numstars;m++){
-    //         cout<<"*";
-    //     }
+        for(int m=0;m<numstars;m++){
+            cout<<"*";
+        }
         
-    //     cout<<endl;
-    // }
+        cout<<endl;
+    }
 
-    // for(int i =0; i<4;i++){ 
+    for(int i =0; i<4;i++){ 
  
-    //     for(int j= 0; j<8;j++){
-    //         if(j<i){
-    //             cout<<" ";
-    //         }
+        for(int j= 0; j<8;j++){
+            if(j<i){
+                cout<<" ";
+            }
 
-    //         else if( j < 8 - i)
-    //         cout<<"*";
-    //     }
-    //     cout<<endl;
-    // } 
+            else if( j < 8 - i)
+            cout<<"*";
+        }
+        cout<<endl;
+    } 
 
 
 
